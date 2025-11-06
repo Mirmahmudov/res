43802487327b1a40555e37761856e5e60e7053dc
Asadbek <asadbekmirmahmudov3@gmail.com>
2025-11-06 22:44:42 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-
-import { BrowserRouter, Routes } from 'react-router'
-import './App.css'
-import Navbar from './navbar/Navbar'
-import { Route } from 'react-router'
-import Home from './home/Home'
+import React from 'react'
 
 function App() {
-
-
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-
-    </>
+    <div>App</div>
   )
 }
 

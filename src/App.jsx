d30eca357d76e74d8e2e8ae08a764f0cd8c44381
Asadbek <asadbekmirmahmudov3@gmail.com>
2025-11-06 22:31:14 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import { BrowserRouter, Routes } from 'react-router'
+import './App.css'
+import Navbar from './navbar/Navbar'
+import { Route } from 'react-router'
+import Home from './home/Home'
+
+function App() {
+ 
+
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/home/Home.jsx
@@ -0,0 +1,197 @@
+import React from 'react'
+import "./Home.css"
+function Home() {
+  return (
+    <>
+<div className="hero">
+<div className="heroinfo">
+    <h1>If not now, when? It’s time to
+    tackle the World’s To-Do List</h1>
+</div>
+</div>
+
+<section className='section1'>
+<div className="conatiner">
+    <div className="s1title">
+    <h1>SUSTAINABLE DEVELOPMENT GOALS</h1>
+    </div>
+       
+        <div className="block1">
+            <div className="box1">
+              <img src="/images/1.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/2.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/3.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/4.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/5.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/6.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/7.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/8.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/9.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/10.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/11.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/12.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/13.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/14.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/15.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/16.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/17.png" alt="" />
+            </div>
+            <div className="box1">
+              <img src="/images/18.png" alt="" />
+            </div>
+            
+            
+
+        </div>
+    
+    <div className="sc1info">
+        <h1>IN 2015, WORLD LEADERS AGREED TO 17 GLOBAL GOALS.</h1>
+        <p>Seven years on, we have made progress, but there is still work to be done, and the Goals are more important than ever. The climate crisis. Ensuring no one goes hungry. Human rights abuses. Extreme poverty. Problems of this scale can be overwhelming, but the Global Goals(also known as the Sustainable Development Goals or SDGs) are the solution to tackling them.
+
+</p>
+<button><h3>START NOW</h3></button>
+    </div>
+
+</div>
+</section>
+
+<section className='section2'> 
+
+<div className="container">
+<div className="picture1">
+    <div className="picinfo">
+        <h1>GET CREATIVE AND SHARE WHAT’S ON YOUR WORLD’S TO-DO LIST</h1>
+        <p>The Global Goals are ambitious, but with great ambition we can achieve great things! To the small organisations advocating for girls to be educated, to the individuals turning plastic waste into something reusable and the businesses innovating to drive change, we see you. If we all take an active part in championing at least one Goal, our society will be better for all of us. What will your contribution be? Use our tool to share what your change advocating for and what’s on your World’s To-Do List.</p>
+        <button><h3>TAKE ACTION</h3></button>
+    </div>
+    <div className="picimg">
+        <img src="/images/creative.png" alt="" />
+    </div>
+</div>
+
+<div className="picture1">
+
+<div className="picimg ">
+        <img src="/images/creative2.png" alt="" />
+    </div>
+    <div className="picinfo  pic2">
+        <h1>WHAT CAN YOU DO?</h1>
+        <p>Every positive action you take, no matter how small, has the power to make a difference. Supporting a clean the oceans campaign, switching to greener products, standing up for justice, and reminding your leaders of the promise of the Global Goals are all good places to start.</p>
+        <button><h3> CAMPAIGNS </h3></button>
+    </div>
+   
+</div>
+
+<div className="picture2">
+    <img src="/images/image.png" alt="" />
+</div>
+</div>
+</section>
+
+<section className='section3'>
+  <div className="container">
+    <div className="sc3title">
+      <h1>WHAT'S HAPPENING</h1>
+    </div>
+    <div className="cards">
+      <div className="card">
+        <p>Jul 22, 2025</p>
+        <h2>Formation of Sustainability and Ethics Committee</h2>
+        <p className='news'>In 2024, Fergana State University has established a Sustainability and Ethics Committee to oversee, develop, and promote sustainable practices across all university operations. The committee is compri...</p>
+
+        <div className="readmore">
+          <button><h1>READ MORE...</h1></button>
+        </div>
+      </div>
+
+      <div className="card">
+        <p>Jul 22, 2025</p>
+        <h2>Formation of Sustainability and Ethics Committee</h2>
+        <p className='news'>In 2024, Fergana State University has established a Sustainability and Ethics Committee to oversee, develop, and promote sustainable practices across all university operations. The committee is compri...</p>
+
+        <div className="readmore">
+          <button><h1>READ MORE...</h1></button>
+        </div>
+      </div>
+
+      <div className="card">
+        <p>Jul 22, 2025</p>
+        <h2>Formation of Sustainability and Ethics Committee</h2>
+        <p className='news'>In 2024, Fergana State University has established a Sustainability and Ethics Committee to oversee, develop, and promote sustainable practices across all university operations. The committee is compri...</p>
+
+        <div className="readmore">
+          <button><h1>READ MORE...</h1></button>
+        </div>
+      </div>
+
+      <div className="card">
+        <p>Jul 22, 2025</p>
+        <h2>Formation of Sustainability and Ethics Committee</h2>
+        <p className='news'>In 2024, Fergana State University has established a Sustainability and Ethics Committee to oversee, develop, and promote sustainable practices across all university operations. The committee is compri...</p>
+
+        <div className="readmore">
+          <button><h1>READ MORE...</h1></button>
+        </div>
+      </div>
+      <div className="card">
+        <p>Jul 22, 2025</p>
+        <h2>Formation of Sustainability and Ethics Committee</h2>
+        <p className='news'>In 2024, Fergana State University has established a Sustainability and Ethics Committee to oversee, develop, and promote sustainable practices across all university operations. The committee is compri...</p>
+
+        <div className="readmore">
+          <button><h1>READ MORE...</h1></button>
+        </div>
+      </div>
+      <div className="card">
+        <p>Jul 22, 2025</p>
+        <h2>Formation of Sustainability and Ethics Committee</h2>
+        <p className='news'>In 2024, Fergana State University has established a Sustainability and Ethics Committee to oversee, develop, and promote sustainable practices across all university operations. The committee is compri...</p>
+
+        <div className="readmore">
+          <button><h1>READ MORE...</h1></button>
+        </div>
+      </div>
+    </div>
+
+    <div className="more">
+      <button><h1>READ MORE</h1></button>
+    </div>
+  </div>
+</section>
+    </>
+  )
+}
+
+export default Home
